5d7c0605101eae140bc298bf6f756d4711d5c1e3
SomMariya <m.som-2030@kpi.ua>
2021-12-10 23:00:06 +0200

    added service generate xlsx

--- src/api/allSizes/allSizes.dto.ts
+++ src/api/allSizes/allSizes.dto.ts
@@ -10,7 +10,7 @@ export const getAllSizesSchema: FastifySchema = {
       footLength: { type: 'number' },
       sex: { type: 'string', enum: ['female', 'male'] }
     },
-    required: ['footLength', 'sex']
+    required: ['footLength', 'sex', 'bustSize', 'waistSize', 'hipsSize']
   },
   params: {
     type: 'object',
--- src/api/allSizes/allSizes.service.ts
+++ src/api/allSizes/allSizes.service.ts
@@ -1,5 +1,8 @@
-import xlsx from 'node-xlsx';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import ExcelJS from 'exceljs';
 import SizeChartShoes from '../sizeChartShoes/sizeChartShoes.service';
+import SizeChartClothes from '../sizeChartClothes/sizeChartClothes.service';
+import BodyType from '../bodyType/bodyType.service';
 
 class AllSizes {
   generatePDF = () => {
@@ -7,6 +10,8 @@ class AllSizes {
   };
 
   shoesService = new SizeChartShoes();
+  clothesService = new SizeChartClothes();
+  bodyTypeService = new BodyType();
 
   async generateXLSXfile(
     bustSize: number,
@@ -15,11 +20,76 @@ class AllSizes {
     footLength: number,
     sex: string,
     unit: string
-  ): Promise<ArrayBuffer> {
+  ): Promise<Buffer> {
     const shoesData = await this.shoesService.getSizeChart(footLength, sex, unit);
-    const columnNames: Array<string> = Object.keys(shoesData);
-    const columnValues: Array<string | number> = Object.values(shoesData);
-    return xlsx.build([{ name: 'mySheetName', data: [columnNames, columnValues] }]);
+    const clothesData = await this.clothesService.getSizeChartClothes(
+      waistSize,
+      hipsSize,
+      bustSize,
+      sex,
+      unit
+    );
+    const bodyTypeData = await this.bodyTypeService.getBodyTypeforMeasurements(
+      waistSize,
+      hipsSize,
+      bustSize,
+      sex,
+      unit
+    );
+    const workbook = new ExcelJS.Workbook();
+    const sheet = workbook.addWorksheet('My Sheet');
+
+    sheet.addTable({
+      name: 'Shoes',
+      ref: 'A1',
+      headerRow: true,
+      style: {
+        theme: 'TableStyleLight2',
+        showRowStripes: true
+      },
+      columns: Object.keys(shoesData).map((key) => ({
+        name: key
+      })),
+      rows: [Object.values(shoesData)]
+    });
+
+    sheet.addTable({
+      name: 'Clothes',
+      ref: 'A4',
+      headerRow: true,
+      style: {
+        theme: 'TableStyleLight2',
+        showRowStripes: true
+      },
+      columns: Object.keys(clothesData).map((key) => ({
+        name: key
+      })),
+      rows: [Object.values(clothesData)]
+    });
+
+    sheet.addTable({
+      name: 'BodyType',
+      ref: 'A7',
+      headerRow: true,
+      style: {
+        theme: 'TableStyleLight2',
+        showRowStripes: true
+      },
+      columns: [{ name: Object.keys(bodyTypeData)[0] }],
+      rows: [[bodyTypeData.bodyType]]
+    });
+
+    sheet.eachRow(function (Row) {
+      Row.eachCell(function (Cell) {
+        Cell.alignment = {
+          vertical: 'middle',
+          horizontal: 'center'
+        };
+      });
+    });
+
+    const buffer: any = await workbook.xlsx.writeBuffer();
+    return buffer;
   }
 }
 export default AllSizes;
