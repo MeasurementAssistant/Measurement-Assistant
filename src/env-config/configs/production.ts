a63d085269273fe80a9f3dfa7a0824fa9677e242
morkvinaValeria <v.morkvina-2025@kpi.ua>
2021-12-11 02:25:38 +0200

    added production config

---
+++ src/env-config/configs/production.ts
@@ -0,0 +1,9 @@
+export const envConfig = {
+  db: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+  }
+};
