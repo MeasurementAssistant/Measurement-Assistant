6c9ac64717e76e623d1abb456af5d5e20a37d96a
SomMariya <m.som-2030@kpi.ua>
2021-12-18 02:31:59 +0200

    changed fastify cors origin

--- index.ts
+++ index.ts
@@ -11,7 +11,10 @@ import { configureI18n } from './src/i18n.config';
 const server = fastify();
 const dbDriver = new PostgresDriver();
 
-server.register(fastifyCors, { origin: true, methods: ['GET', 'POST', 'PUT'] });
+server.register(fastifyCors, {
+  origin: 'https://measurementassistant.github.io/Measurement-Assistant-Portal/',
+  methods: ['GET', 'POST', 'PUT']
+});
 server.register(swagger, swaggerConfig);
 
 server.route(routes.getShoesSize);
