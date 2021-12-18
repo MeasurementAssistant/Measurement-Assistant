b65aca5353fdaf4770bfba860fbfab54680c45ba
SomMariya <m.som-2030@kpi.ua>
2021-12-18 02:39:43 +0200

    changed fastify cors origin v3

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const server = fastify();
 const dbDriver = new PostgresDriver();
 
 server.register(fastifyCors, {
-  origin: 'https://measurment-assistant.herokuapp.com',
+  origin: ['https://measurment-assistant.herokuapp.com', 'https://measurementassistant.github.io'],
   allowedHeaders: 'Content-Type,Authorization',
   methods: ['GET', 'POST', 'PUT']
 });
